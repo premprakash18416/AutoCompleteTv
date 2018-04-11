ba554cc36d7ab3e5369609e029f3bb3a4fb4ee59
prem <premprakash184@gmail.com>
2018-04-11 11:45:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prem/autocompletetv/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.prem.autocompletetv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] countries = new String[]{"Belgium", "France", "Italy", "Germany", "Spain"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,countries);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setThreshold(2);
+    }
+}
